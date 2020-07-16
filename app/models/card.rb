ecd6a06177fa08410b5532f34df8152a12998a83
marshall405 <marshall.slemp@gmail.com>
2020-07-16 18:21:20 -0500

    add trade resource and ability to create new trade

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ApplicationRecord
     belongs_to :user
+
+    # Can only be offered in one trade at a time
+    belongs_to :trade
+
+    # Can be the requested card in many trades
+    has_many :requested_trades, foreign_key: :requested_card_id, class_name: "Trade"
 end
